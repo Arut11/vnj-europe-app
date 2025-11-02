7c075a81d41aa9a87115ee593aa6f00b625ef686
Arutyun Sargatyan <Sargatian2017@yandex.ru>
2025-11-02 18:13:59 +0300

    Add Yandex verification file

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/vnj-europe-app/',
-  build: {
-    outDir: 'dist'
-  },
-  assetsInclude: ['**/*.html']  // ← переместите сюда!
 });
